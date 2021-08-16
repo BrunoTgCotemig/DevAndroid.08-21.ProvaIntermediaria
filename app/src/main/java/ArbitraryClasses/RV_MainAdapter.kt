980e3ec36ad3441bd952681d2c07ad073b9d3273
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-08-16 12:27:41 -0300

    in progress 3

--- app/src/main/java/ArbitraryClasses/RV_MainAdapter.kt
+++ app/src/main/java/ArbitraryClasses/RV_MainAdapter.kt
@@ -1,26 +1,31 @@
 package ArbitraryClasses
 
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.a2etapa_provaintermediaria.R
 
 class RV_MainAdapter (var todos:List<RV_MainAdapter_todo>): RecyclerView.Adapter<RV_MainAdapter.Adapter1>()
 {// -- Code
     inner class Adapter1(itemView: View) : RecyclerView.ViewHolder(itemView)
+                    //This stands for the to-do ViewHolder
 
-    
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter1 {
-        TODO("Not yet implemented")
+
+        // Here, we inflate the .xml file to actually get it as a view
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rv_itemstructure, parent, false) // I'm not very sure of what this does...
+        return Adapter1(view)
     }
 
     override fun onBindViewHolder(holder: Adapter1, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemView.apply{}
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return todos.size
     }
 }// -- Code
