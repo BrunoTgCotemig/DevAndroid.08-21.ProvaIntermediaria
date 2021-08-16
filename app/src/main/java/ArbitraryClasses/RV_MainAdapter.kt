3284a66288f77b9a07a5ccaeff9020d835ff7799
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-08-16 11:34:33 -0300

    in progress 2

--- app/src/main/java/ArbitraryClasses/RV_MainAdapter.kt
+++ app/src/main/java/ArbitraryClasses/RV_MainAdapter.kt
@@ -1,4 +1,26 @@
 package ArbitraryClasses
 
-class RV_MainAdapter {
-}
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RV_MainAdapter (var todos:List<RV_MainAdapter_todo>): RecyclerView.Adapter<RV_MainAdapter.Adapter1>()
+{// -- Code
+    inner class Adapter1(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter1 {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Adapter1, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}// -- Code
---
+++ app/src/main/java/ArbitraryClasses/RV_MainAdapter_todo.kt
@@ -0,0 +1,9 @@
+package ArbitraryClasses
+
+data class RV_MainAdapter_todo(
+    val ProductName: String,
+    val ProductDescription: String,
+    val ProductValue: String,
+)
+{
+}
