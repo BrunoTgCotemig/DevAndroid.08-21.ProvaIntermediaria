dbd05ffea2301d05e30bdc9b5ec3c93c85968d86
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-08-16 18:19:05 -0300

    in progress 5

--- app/src/main/java/com/example/a2etapa_provaintermediaria/MainActivity.kt
+++ app/src/main/java/com/example/a2etapa_provaintermediaria/MainActivity.kt
@@ -5,6 +5,7 @@ import ArbitraryClasses.RV_MainAdapter
 import ArbitraryClasses.RV_MainAdapter_todo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -27,17 +28,20 @@ class MainActivity : AppCompatActivity()
         RV_MainAdapter_todo("Curso: Flutter"," ~~ Muito melhor que React", "Valor: $20.00"),
         RV_MainAdapter_todo("Curso: Angular"," ~~ Você nunca o intenderá completamente", "Valor: $30.00"),
         RV_MainAdapter_todo("Curso: C#"," ~~ Sem ASP.NET", "Valor: $44.00"),
-        RV_MainAdapter_todo("Curso: F#"," ~~ Melhor que orientação a objetos", "Valor: $50.00"),
+        RV_MainAdapter_todo("Curso: F#"," ~~ Programação funcional > orientada a objetos", "Valor: $50.00"),
         RV_MainAdapter_todo("Curso: Rust"," ~~ Melhor que c++ ..?", "Valor: $66.00"),
         RV_MainAdapter_todo("Curso: Java"," ~~ Melhor que c#", "Valor: $70.00"),
         RV_MainAdapter_todo("Curso: NodeJs"," ~~ Como fazer variaveis globais", "Valor: $88.00"),
         RV_MainAdapter_todo("Curso: AppInventor"," ~~ A MELHOR FERRAMENTA PARA CRIAÇÃO DE APPS", "Valor: $90.00"),
         RV_MainAdapter_todo("Curso: C++"," ~~ RAM sobrecarregada", "Valor: $1100.00"),
-        RV_MainAdapter_todo("Curso: Arduino"," ~~ Porque? Porque SIM!", "Valor: $2000.00"),
+        RV_MainAdapter_todo("Curso: C"," ~~ Programação procedural > Orientada a objetos", "Valor: $2000.00"),
     )
 
-        val at_adapter = RV_MainAdapter(todoList)
-        findViewById<RecyclerView>(R.id.Main_RecyclerView).adapter = at_adapter
+        val RV_MainAdapter = RV_MainAdapter(todoList)
+        val Main_RecyclerView = findViewById<RecyclerView>(R.id.Main_RecyclerView)
+
+        Main_RecyclerView.adapter = RV_MainAdapter
+        Main_RecyclerView.layoutManager = LinearLayoutManager(this)
 
     }
 }
