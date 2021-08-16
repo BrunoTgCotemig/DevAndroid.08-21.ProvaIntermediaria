863e0f38c5dbeff8fd89126b7e2fea666e6dd7fe
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-08-16 11:02:01 -0300

    in progress 1

---
+++ app/src/main/java/ArbitraryClasses/RV_MainAdapter.kt
@@ -0,0 +1,4 @@
+package ArbitraryClasses
+
+class RV_MainAdapter {
+}
--- app/src/main/java/com/example/a2etapa_provaintermediaria/MainActivity.kt
+++ app/src/main/java/com/example/a2etapa_provaintermediaria/MainActivity.kt
@@ -1,10 +1,24 @@
 package com.example.a2etapa_provaintermediaria
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+class MainActivity : AppCompatActivity()
+{
+   /*
+    * AUTOR DO PROJETO
+    * MATRÍCULA: 11900989
+    * NÚMERO DE CHAMADA: 10
+    * NOME: Bruno Teles Galvão
+    */
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
